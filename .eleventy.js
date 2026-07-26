fd822ed2c531ec4b4ca571d207be826674df02c5
Luis_Alves <33226205+LuisAlves18@users.noreply.github.com>
2026-07-26 23:25:30 +0100

    Create .eleventy.js

---
+++ .eleventy.js
@@ -0,0 +1,36 @@
+module.exports = function (eleventyConfig) {
+  // Ficheiros estáticos passados diretamente para o output
+  eleventyConfig.addPassthroughCopy({ "src/css": "css" });
+
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return new Date(dateObj).toLocaleDateString("pt-PT", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  });
+
+  // Coleção do blog, com filtro por idioma
+  eleventyConfig.addCollection("blogPt", (collectionApi) => {
+    return collectionApi
+      .getFilteredByGlob("content/blog/**/*_pt.md")
+      .sort((a, b) => b.date - a.date);
+  });
+
+  eleventyConfig.addCollection("blogEn", (collectionApi) => {
+    return collectionApi
+      .getFilteredByGlob("content/blog/**/*_en.md")
+      .sort((a, b) => b.date - a.date);
+  });
+
+  return {
+    dir: {
+      input: "content",
+      includes: "../_includes",
+      data: "../_data",
+      output: "_site",
+    },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+  };
+};
